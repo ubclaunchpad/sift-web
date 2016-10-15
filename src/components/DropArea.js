2b88edfb863a90879a8ba92b3612edbd2b775b4d
Alek Hrycaiko <alekhrycaiko@dhcp-206-87-222-213.ubcsecure.wireless.ubc.ca>
2016-10-15 13:45:01 -0700

    Further changes to files import

--- src/components/DropArea.js
+++ src/components/DropArea.js
@@ -5,7 +5,8 @@ class DropArea extends Component {
 	static displayName = 'DropArea';
 
 	state = {
-		count : 0
+		count : 0,
+		files: []
 	}
 
 	onDrop (acceptedFile) {
@@ -22,7 +23,7 @@ class DropArea extends Component {
 					<button type="button" onClick={this.onOpenClick}>
 						Open Dropzone
 					</button>
-					{this.state && this.state.files && this.state.files.length > 0 ? <div>
+					{this.state.files.length > 0 ? <div>
 						<h2> Uploading {this.state.files.length} files...</h2>
 						<div> {this.state.files.map(file => <img src={file.preview} /> )}</div>
 					</div>
