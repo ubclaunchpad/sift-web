5c73647e1a7fea36a6365ea8a193b26b68788a1c
Matthew <siunami.matt@gmail.com>
2016-10-15 13:52:08 -0700

    Fixed tests and formatting for eslint

--- src/components/sample/__tests__/button.test.js
+++ src/components/sample/__tests__/button.test.js
@@ -2,9 +2,9 @@ import Button from './../Button';
 import React from 'react';
 import {shallow} from 'enzyme';
 
-describe ('<Button/>', () => {
-  it('Should render a button', () => {
-    const wrapper = shallow(<Button/>);
-    expect(wrapper.contains(<Text>Testing</Text>)).to.equal(true);
-  });
+describe('<Button />', () => {
+	it('Should render a button', () => {
+		const wrapper = shallow(<Button/>);
+		expect(wrapper.find('Button').length).toEqual(1);
+	});
 });
--- src/components/sample/button.js
+++ src/components/sample/button.js
@@ -2,35 +2,35 @@ import React, {Component} from 'react';
 
 class Button extends Component {
 
-  static displayName = 'Button';
+	static displayName = 'Button';
 
-  static propTypes = {
-    isDisabled: PropTypes.bool
-  };
+	static propTypes = {
+		isDisabled: PropTypes.bool
+	};
 
-  static defaultProps = {
-    isDisabled: false
-  };
+	static defaultProps = {
+		isDisabled: false
+	};
 
-  render() {
-    return (
-      <div>
-        <p>Testing</p>
-        <button onClick={this._onClick}>
-          Submit File
-        </button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<p>Testing</p>
+				<button onClick={this._onClick}>
+					Submit File
+				</button>
+			</div>
+		);
+	}
 
-  _onClick = () => {
-    if (isDisabled){
-      alert("Upload a file first.")
-    } else {
-      // Upload file
-    }
-  }
+	_onClick = () => {
+		if (this.PropTypes.isDisabled){
+			console.log('Upload a file first');
+		} else {
+			// Upload file
+		}
+	}
 }
 
-export default Counter;
-export counter;
+export default Button;
+export {Button};
