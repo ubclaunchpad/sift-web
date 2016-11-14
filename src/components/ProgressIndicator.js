527232799ba05ab55c6fed5f8fb4fc21f5b31316
Andrew Bui <anhduc130@live.com>
2016-11-13 16:15:09 -0800

    Updated Progress Indicator

--- src/components/ProgressIndicator.js
+++ src/components/ProgressIndicator.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const ProgressIndicator = () => (
-	<div className= "progressIndicator"></div>
+	<div className="progressIndicator"></div>
 );
+
+ProgressIndicator.displayName = 'ProgressIndicator';
 
 export default ProgressIndicator;
